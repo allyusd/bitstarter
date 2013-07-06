0c7d4ef03f0d938fc0bfcd980d8763d4bdd6f2f1
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2013-07-06 21:13:07 +0000

    bug fix

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   fs.readFile('index.html', function(err, data) {
     if (err) throw err;
-    var buf = new Buffer(dat);
+    var buf = new Buffer(data);
     response.send(buf.toString());
   });
 });
