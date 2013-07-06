765d607c9deb53bbcf4af7ebaceafe19a9db1741
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2013-07-06 21:04:20 +0000

    Update web.js, read from file

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  fs.readFile(file, function(err, data) {
+    if (err) throw err;
+    var buf = new Buffer(dat);
+    response.send(buf.toString());
+  });
 });
 
 var port = process.env.PORT || 5000;
